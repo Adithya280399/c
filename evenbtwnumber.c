e22955f7b54c70b0452295fa54732a5c136b1b3e
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-07-26 00:10:04 -0900

    Create evenbtwnumber.c

---
+++ evenbtwnumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a1,a2,rem,i;
+printf("enter the first number in the range,\n");
+scanf("%d",&a1);
+printf("enter the last number in the range,\n");
+scanf("%d",&a2);
+printf("the even numbers are");
+for(i=a1;i<=a2;i++){
+	rem=i%2;
+	if(rem==0)
+	printf(" \n %d",i);
+}
+return 0;
+}
