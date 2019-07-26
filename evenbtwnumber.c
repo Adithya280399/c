46b5b12f57deca3991d6a5439b8a6d2ed42dbd43
Adithya280399 <47312447+Adithya280399@users.noreply.github.com>
2019-07-26 00:24:31 -0900

    Update evenbtwnumber.c

--- evenbtwnumber.c
+++ evenbtwnumber.c
@@ -2,11 +2,8 @@
 int main()
 {
 int a1,a2,rem,i;
-printf("enter the first number in the range,\n");
 scanf("%d",&a1);
-printf("enter the last number in the range,\n");
 scanf("%d",&a2);
-printf("the even numbers are");
 for(i=a1+1;i<a2;i++){
 	rem=i%2;
 	if(rem==0)
